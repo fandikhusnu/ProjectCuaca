4e87dc7968e2f17ea79fcedae4681611df380395
ahmaddevara <devaraahmad@gmail.com>
2016-11-20 20:42:33 +0700

    Yahoo Weather Service

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl502112029/cuaca/listener/YahooWeatherService.java
@@ -0,0 +1,110 @@
+package id.sch.smktelkom_mlg.project.xirpl502112029.cuaca.listener;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import id.sch.smktelkom_mlg.project.xirpl502112029.cuaca.data.Channel;
+
+/**
+ * Created by microsoft on 11/20/2016.
+ */
+
+public class YahooWeatherService {
+    private WeatherServiceListener listener;
+    private Exception error;
+    private String temperatureUnit = "C";
+
+    public YahooWeatherService(WeatherServiceListener listener) {
+        this.listener = listener;
+    }
+
+    public String getTemperatureUnit() {
+        return temperatureUnit;
+    }
+
+    public void setTemperatureUnit(String temperatureUnit) {
+        this.temperatureUnit = temperatureUnit;
+    }
+
+    public void refreshWeather(String location) {
+
+        new AsyncTask<String, Void, Channel>() {
+            @Override
+            protected Channel doInBackground(String[] locations) {
+
+                String location = locations[0];
+
+                Channel channel = new Channel();
+
+                String unit = getTemperatureUnit().equalsIgnoreCase("f") ? "f" : "c";
+
+                String YQL = String.format("select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"%s\") and u='" + unit + "'", location);
+
+                String endpoint = String.format("https://query.yahooapis.com/v1/public/yql?q=%s&format=json", Uri.encode(YQL));
+
+                try {
+                    URL url = new URL(endpoint);
+
+                    URLConnection connection = url.openConnection();
+                    connection.setUseCaches(false);
+
+                    InputStream inputStream = connection.getInputStream();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    JSONObject data = new JSONObject(result.toString());
+
+                    JSONObject queryResults = data.optJSONObject("query");
+
+                    int count = queryResults.optInt("count");
+
+                    if (count == 0) {
+                        error = new LocationWeatherException("No weather information found for " + location);
+                        return null;
+                    }
+
+                    JSONObject channelJSON = queryResults.optJSONObject("results").optJSONObject("channel");
+                    channel.populate(channelJSON);
+
+                    return channel;
+
+                } catch (Exception e) {
+                    error = e;
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Channel channel) {
+
+                if (channel == null && error != null) {
+                    listener.serviceFailure(error);
+                } else {
+                    listener.serviceSuccess(channel);
+                }
+
+            }
+
+        }.execute(location);
+    }
+
+    public class LocationWeatherException extends Exception {
+        public LocationWeatherException(String detailMessage) {
+            super(detailMessage);
+        }
+    }
+}
