7b1be4805019fc45ee958bb0f7c3d729222e5b67
Samudra~ <muhamad_samudra_24rpl@student.smktelkom-mlg.sch.id>
2016-11-20 20:54:58 +0700

    GoogleMapsGeocodingService & WeatherCacheService

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl502112029/cuaca/service/GoogleMapsGeocodingService.java
@@ -0,0 +1,97 @@
+package id.sch.smktelkom_mlg.project.xirpl502112029.cuaca.service;
+
+import android.location.Location;
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import id.sch.smktelkom_mlg.project.xirpl502112029.cuaca.data.LocationResult;
+import id.sch.smktelkom_mlg.project.xirpl502112029.cuaca.listener.GeocodingServiceListener;
+
+/**
+ * Created by Samudra~ on 20/11/2016.
+ */
+
+public class GoogleMapsGeocodingService {
+    private static final String API_KEY = "";
+    private GeocodingServiceListener listener;
+    private Exception error;
+
+    public GoogleMapsGeocodingService(GeocodingServiceListener listener) {
+        this.listener = listener;
+    }
+
+    public void refreshLocation(Location location) {
+        new AsyncTask<Location, Void, LocationResult>() {
+            @Override
+            protected LocationResult doInBackground(Location... locations) {
+
+                Location location = locations[0];
+
+                String endpoint = String.format("https://maps.googleapis.com/maps/api/geocode/json?latlng=%s,%s&key=%s", location.getLatitude(), location.getLongitude(), API_KEY);
+
+                try {
+                    URL url = new URL(endpoint);
+
+                    URLConnection connection = url.openConnection();
+                    connection.setUseCaches(false);
+
+                    InputStream inputStream = connection.getInputStream();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    JSONObject data = new JSONObject(result.toString());
+
+                    JSONArray results = data.optJSONArray("results");
+
+                    if (results.length() == 0) {
+                        error = new ReverseGeolocationException("Could not reverse geocode " + location.getLatitude() + ", " + location.getLongitude());
+
+                        return null;
+                    }
+
+                    LocationResult locationResult = new LocationResult();
+                    locationResult.populate(results.optJSONObject(0));
+
+                    return locationResult;
+
+                } catch (Exception e) {
+                    error = e;
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(LocationResult location) {
+
+                if (location == null && error != null) {
+                    listener.geocodeFailure(error);
+                } else {
+                    listener.geocodeSuccess(location);
+                }
+
+            }
+
+        }.execute(location);
+    }
+
+    private class ReverseGeolocationException extends Exception {
+        public ReverseGeolocationException(String detailMessage) {
+            super(detailMessage);
+        }
+    }
+}
+
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl502112029/cuaca/service/WeatherCacheService.java
@@ -0,0 +1,102 @@
+package id.sch.smktelkom_mlg.project.xirpl502112029.cuaca.service;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import org.json.JSONObject;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import id.sch.smktelkom_mlg.project.xirpl502112029.cuaca.R;
+import id.sch.smktelkom_mlg.project.xirpl502112029.cuaca.data.Channel;
+import id.sch.smktelkom_mlg.project.xirpl502112029.cuaca.listener.WeatherServiceListener;
+
+/**
+ * Created by Samudra~ on 20/11/2016.
+ */
+
+public class WeatherCacheService {
+    private final String CACHED_WEATHER_FILE = "weather.data";
+    private Context context;
+    private Exception error;
+
+    public WeatherCacheService(Context context) {
+        this.context = context;
+    }
+
+    public void save(Channel channel) {
+        new AsyncTask<Channel, Void, Void>() {
+            @Override
+            protected Void doInBackground(Channel[] channels) {
+
+                FileOutputStream outputStream;
+
+                try {
+                    outputStream = context.openFileOutput(CACHED_WEATHER_FILE, Context.MODE_PRIVATE);
+                    outputStream.write(channels[0].toJSON().toString().getBytes());
+                    outputStream.close();
+
+                } catch (IOException e) {
+                    // TODO: file save operation failed
+                }
+
+                return null;
+            }
+        }.execute(channel);
+    }
+
+    public void load(final WeatherServiceListener listener) {
+
+        new AsyncTask<WeatherServiceListener, Void, Channel>() {
+            private WeatherServiceListener weatherListener;
+
+            @Override
+            protected Channel doInBackground(WeatherServiceListener[] serviceListeners) {
+                weatherListener = serviceListeners[0];
+                try {
+                    FileInputStream inputStream = context.openFileInput(CACHED_WEATHER_FILE);
+
+                    StringBuilder cache = new StringBuilder();
+                    int content;
+                    while ((content = inputStream.read()) != -1) {
+                        cache.append((char) content);
+                    }
+
+                    inputStream.close();
+
+                    JSONObject jsonCache = new JSONObject(cache.toString());
+
+                    Channel channel = new Channel();
+                    channel.populate(jsonCache);
+
+                    return channel;
+
+                } catch (FileNotFoundException e) { // cache file doesn't exist
+                    error = new CacheException(context.getString(R.string.cache_exception));
+                } catch (Exception e) {
+                    error = e;
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Channel channel) {
+                if (channel == null && error != null) {
+                    weatherListener.serviceFailure(error);
+                } else {
+                    weatherListener.serviceSuccess(channel);
+                }
+            }
+        }.execute(listener);
+    }
+
+    public class CacheException extends Exception {
+        public CacheException(String detailMessage) {
+            super(detailMessage);
+        }
+    }
+}
